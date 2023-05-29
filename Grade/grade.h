7e4be2989bf99adb2bdf8df4dacd790d5ba280cb
Francisco Antonio da Luz Filho <39159963+fantonioluz@users.noreply.github.com>
2023-05-29 18:12:24 -0300

    Funcionalidade de adicionar note e feedback

---
+++ Grade/grade.h
@@ -0,0 +1,10 @@
+#ifndef GRADE_H
+#define GRADE_H
+#include "student.h"
+
+void chooseStudent();
+void submitGrade(Student student, char* studentName);
+void saveFile(Student student, char* studentName);
+void viewGrade();
+
+#endif
---
+++ Grade/student.h
@@ -0,0 +1,7 @@
+typedef struct {
+    char name[50];
+    int id;
+    char activities[50];
+    float grade;
+    char feedback[250];
+} Student;
