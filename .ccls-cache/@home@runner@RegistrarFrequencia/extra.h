70ed3c23966d14da4356c056e462b3dcca082028
Francisco Antonio da Luz Filho <39159963+fantonioluz@users.noreply.github.com>
2023-06-12 22:14:45 -0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@RegistrarFrequencia/extra.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTRA_H
-#define EXTRA_H
-
-/* Funções extra execução do código */
-void converterMaiuscula(char *str);
-void delay(int seconds);
-void exibirQRCode();
-
-#endif /* EXTRA_H */
--- .ccls-cache/@home@runner@RegistrarFrequencia/student.c
+++
@@ -1,7 +0,0 @@
-typedef struct {
-    char name[50];
-    int id;
-    char activities[50];
-    float grade;
-    char feedback[250];
-} Student;
